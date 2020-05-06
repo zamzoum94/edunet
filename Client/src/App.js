e780fbd1c10526bf254a084db3f7c8852a3f69e2
zamzoum94 <mzemzemi9@gmail.com>
2020-05-06 22:52:02 +0100

    teachers added

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 import Home from './modules/home';
-import Header from './modules/navbar'
+import Header from './modules/navbar';
+import Courses from './modules/courses';
 
 class App extends React.Component {
   render(){
     return (
       <div className='container'>
-        <Header/>
-        <Home/>
+        <Courses/>
     </div>
     );
   }
---
+++ Client/src/modules/courses.js
@@ -0,0 +1,42 @@
+
+import React from 'react';
+
+let obj = {name : 'book', desciption:'hello', img : 'https://i.ytimg.com/vi/SQJrYw1QvSQ/maxresdefault.jpg'};
+let arr = [], max = 10;
+for(let i = 0; i < max; i++){
+    arr.push(obj);
+}
+
+
+export default class Courses extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div>
+                <div className='row'>
+                    {arr.map((element, index)=>{
+                        return (
+                            <div className='col-md-4'>
+                                <div className='card'>
+                                    <img src={element.img} className='card-img-top' alt='cat'></img>
+                                    <div className='card-body'>
+                                        <h4 className='card-title'>
+                                            {element.name}
+                                        </h4>
+                                        <p className='card-text'>
+                                            {element.desciption}
+                                        </p>
+                                        <button type='submit' className='btn btn-primary'>Check course</button>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+        )
+    }
+}
