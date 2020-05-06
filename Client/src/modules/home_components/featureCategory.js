9c99b8164ff416ff4d407ff974cee8d9d0d6eedc
zamzoum94 <mzemzemi9@gmail.com>
2020-05-06 13:42:27 +0100

    category and teachers added

--- Client/src/modules/home_components/featureCategory.js
+++ Client/src/modules/home_components/featureCategory.js
@@ -10,7 +10,24 @@ export default class FeatureCategory extends React.Component {
     render(){
         return(
         <div>
-
-        </div>);
+            <div className='row'>
+                <div className='col-md-2 categoryBorder'>
+                    <h2> Category goes here</h2>
+                </div>
+                <div className='col-md-2 categoryBorder'>
+                    <h2> Category goes here</h2>
+                </div>
+                <div className='col-md-2 categoryBorder'>
+                    <h2> Category goes here</h2>
+                </div>
+                <div className='col-md-2 categoryBorder'>
+                    <h2> Category goes here</h2>
+                </div>
+                <div className='col-md-2 categoryBorder'>
+                    <h2> Category goes here</h2>
+                </div>
+            </div>
+        </div>
+        );
     }
 }
--- Client/src/modules/home_components/featureTeachers.js
+++ Client/src/modules/home_components/featureTeachers.js
@@ -10,7 +10,41 @@ export default class FeatureTeachers extends React.Component {
     render(){
         return(
             <div>
-    
+                <div className='row'>
+                    <div className='col-md-3 offset-md-1'>
+                        <div className='card'>
+                            <img src='https://www.travelcontinuously.com/wp-content/uploads/2018/04/empty-avatar.png' className='card-img-top'></img>
+                            <div className='card-body'>
+                                <div className='card-text'>
+                                    Text goes here
+                                </div>
+                                <a href="#" class="card-link">Teacher link</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-md-3 offset-md-1'>
+                        <div className='card'>
+                            <img src='https://www.travelcontinuously.com/wp-content/uploads/2018/04/empty-avatar.png' className='card-img-top'></img>
+                            <div className='card-body'>
+                                <div className='card-text'>
+                                    Text goes here
+                                </div>
+                                <a href="#" class="card-link">Teacher link</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-md-3 offset-md-1'>
+                        <div className='card'>
+                            <img src='https://www.travelcontinuously.com/wp-content/uploads/2018/04/empty-avatar.png' className='card-img-top'></img>
+                            <div className='card-body'>
+                                <div className='card-text'>
+                                    Text goes here
+                                </div>
+                                <a href="#" class="card-link">Teacher link</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
