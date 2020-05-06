5e58683638b609ba9765adfb9f6a88a62e667877
zamzoum94 <mzemzemi9@gmail.com>
2020-05-06 01:49:43 +0100

    routes v1

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,1 +1,10 @@
-const router = require('express').Router()
+const router = require('express').Router();
+
+const handler = require('../handlers');
+
+router.post('/signup', handler.signeUp);
+
+router.post('/login', handler.login);
+
+
+module.exports = router;
