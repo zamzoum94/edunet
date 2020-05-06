2b68f52d68fc6bc4ee429ff699b3b7aef7e8e82b
taha0505 <taha.khelfa@hotmail.fr>
2020-05-06 22:29:07 +0100

    navbar-

--- Client/src/modules/home_components/featureChoose.js
+++ Client/src/modules/home_components/featureChoose.js
@@ -2,46 +2,46 @@ import React from 'react';
 export default class ChooseUs extends React.Component{
     render(){
         return(
-            <div classNameNameName="featureChoose">
-                <div  classNameNameName="carousel slide" data-ride="carousel">
-                    <ol classNameNameName="carousel-indicators">
-                        <li data-target="#carouselExampleCaptions" data-slide-to="0" classNameNameName="active"></li>
+            <div className="featureChoose">
+                <div  className="carousel slide" data-ride="carousel">
+                    <ol className="carousel-indicators">
+                        <li data-target="#carouselExampleCaptions" data-slide-to="0" className="active"></li>
                         <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
                         <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
                     </ol>
-                    <div classNameNameName="carousel-inner">
-                        <div classNameNameName="carousel-item active">
+                    <div className="carousel-inner">
+                        <div className="carousel-item active">
                         <img src="https://d194ip2226q57d.cloudfront.net/images/Best-Float-Trips_Niangua_CO-Shutterstock.original.jpg
-" classNameNameName="d-block w-100" alt="..."></img>
-                        <div classNameNameName="carousel-caption d-none d-md-block">
+" className="d-block w-100" alt="..."></img>
+                        <div className="carousel-caption d-none d-md-block">
                             <h5>First slide label</h5>
                             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                         </div>
                         </div>
-                        <div classNameNameName="carousel-item">
+                        <div className="carousel-item">
                         <img src="https://d194ip2226q57d.cloudfront.net/images/Best-Float-Trips_Niangua_CO-Shutterstock.original.jpg
-" classNameNameName="d-block w-100" alt="..."></img>
-                        <div classNameNameName="carousel-caption d-none d-md-block">
+" className="d-block w-100" alt="..."></img>
+                        <div className="carousel-caption d-none d-md-block">
                             <h5>Second slide label</h5>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                         </div>
                         </div>
-                        <div classNameNameName="carousel-item">
+                        <div className="carousel-item">
                         <img src="https://d194ip2226q57d.cloudfront.net/images/Best-Float-Trips_Niangua_CO-Shutterstock.original.jpg
-" classNameNameName="d-block w-100" alt="..."></img>
-                        <div classNameNameName="carousel-caption d-none d-md-block">
+" className="d-block w-100" alt="..."></img>
+                        <div className="carousel-caption d-none d-md-block">
                             <h5>Third slide label</h5>
                             <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
                         </div>
                         </div>
                     </div>
-                    <a classNameNameName="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-                        <span classNameNameName="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span classNameNameName="sr-only">Previous</span>
+                    <a className="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="sr-only">Previous</span>
                     </a>
-                    <a classNameNameName="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-                        <span classNameNameName="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span classNameNameName="sr-only">Next</span>
+                    <a className="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="sr-only">Next</span>
                     </a>
                     </div>
 
