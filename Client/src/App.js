6d7462302a16ed6f0657b810e5e8f51229ac9558
taha0505 <taha.khelfa@hotmail.fr>
2020-05-06 23:02:53 +0100

    teachers component added

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,12 +3,13 @@ import React from 'react';
 import Home from './modules/home';
 import Header from './modules/navbar';
 import Courses from './modules/courses';
+import Teachers from './modules/teachers/teachers';
 
 class App extends React.Component {
   render(){
     return (
       <div className='container'>
-        <Courses/>
+        <Teachers/>
     </div>
     );
   }
