a9bcb83bfc98894005cecd1c8fa9e96e54a165ed
zamzoum94 <mzemzemi9@gmail.com>
2020-05-07 02:18:53 +0100

    teacher componenet

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,12 +3,13 @@ import React from 'react';
 import Home from './modules/home';
 import Header from './modules/navbar';
 import Courses from './modules/courses';
+import Teacher from './modules/teacher'
 
 class App extends React.Component {
   render(){
     return (
       <div className='container'>
-        <Courses/>
+        <Teacher/>
     </div>
     );
   }
