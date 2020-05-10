bb823d42d322a1443fc3ea89d57bf4732f23c2b3
maminez23 <60395940+maminez23@users.noreply.github.com>
2020-05-11 01:03:54 +0200

    database videos

--- Client/src/modules/home_components/featureCategory.js
+++ Client/src/modules/home_components/featureCategory.js
@@ -1,40 +1,45 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 export default class FeatureCategory extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            categories : []
+        this.state = {
+            categories: []
         };
         this.fetchCategories();
     }
 
-    fetchCategories(){
+    fetchCategories() {
         fetch('http://localhost:8080/category', {
-            method : 'GET'
+            method: 'GET'
         })
-            .then(res =>{
+            .then(res => {
                 return res.text()
             })
-            .then(data =>{
-            this.setState({
-                categories : JSON.parse(data).map((element, idx) =>{
-                    return element.name;
+            .then(data => {
+                this.setState({
+                    categories: JSON.parse(data).map((element, idx) => {
+                        return element.name;
+                    })
                 })
-            })
 
-        })
+            })
     }
 
-    render(){
-        return(
-        <div className="featureCategory">
-            <div className='row'>
-                {this.state.categories.map((element,idx) =>{
-                    return(
-                        <div className='col-md-2 categoryBorder'>
-                            <Link to={'searchbycategory/'+(idx+1)}><h2>{element}</h2></Link>
-                        </div>
-                    )
-                })
-                }
+    render() {
+        return (
+            <div className="featureCategory">
+                <div className='row'>
+                    {this.state.categories.map((element, idx) => {
+                        return (
+                            <div className='col-md-2 categoryBorder'>
+                                <Link to={'searchbycategory/' + (idx + 1)}><h2>{element}</h2></Link>
+                            </div>
+                        )
+                    })
+                    }
+                </div>
+            </div>
+        )
+    }
+}
